2ea1c48efcb168a8df5409d2ca772d16339de4ee
curious725 <curious725@gmail.com>
2017-01-16 21:29:32 +0200

    delete sqlite3 from default database in settings

--- official_tutorial_django/official_tutorial_django/settings.py
+++ official_tutorial_django/official_tutorial_django/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'official_tutorial_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': '',
+        'NAME': '',
     }
 }
 

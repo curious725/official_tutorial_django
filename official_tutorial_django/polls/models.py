359204c1b1eee92b60485b62078b7531d7144fd9
curious725 <curious725@gmail.com>
2017-01-17 22:11:53 +0200

    add representation for models

--- official_tutorial_django/polls/models.py
+++ official_tutorial_django/polls/models.py
@@ -1,15 +1,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible  # only if you need to support Python 2
 class Question(models.Model):
     question_text = models.CharField(
         max_length=200
     )
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
 
+
+@python_2_unicode_compatible  # only if you need to support Python 2
 class Choice(models.Model):
     question = models.ForeignKey(
         Question, on_delete=models.CASCADE
@@ -21,4 +27,5 @@ class Choice(models.Model):
         default=0
     )
 
-
+    def __str__(self):
+        return self.choice_text

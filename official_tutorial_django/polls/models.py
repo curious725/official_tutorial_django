09245ca4de545c1022722d42210fca1a75d7819b
curious725 <curious725@gmail.com>
2017-01-19 14:23:25 +0200

    add method to Question model

--- official_tutorial_django/polls/models.py
+++ official_tutorial_django/polls/models.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
+import datetime
 
 from django.db import models
+from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
 
 
@@ -10,6 +12,9 @@ class Question(models.Model):
         max_length=200
     )
     pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     def __str__(self):
         return self.question_text
